3afa61aac036bdf104a62cf0038052ee18f0bc53
Moshin Zamani <mohsenzamanist@gmail.com>
2023-04-08 20:51:03 +0100

    Set logFlag

--- frontend/src/component/NavBar.jsx
+++ frontend/src/component/NavBar.jsx
@@ -5,60 +5,25 @@ function NavBar() {
   const login = useSelector((state) => state.log.value);
 
   if (login) {
-    return (
-      <>
-        <nav className="navbar navbar-expand-lg rounded mt-2">
-          <div className="container-fluid">
-            <Link to="/">
-              <p className="logo"></p>
-            </Link>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <Link to="/" className="nav-link">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="type" className="nav-link">
-                    Type
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="brand" className="nav-link">
-                    Brand
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="signup" className="nav-link">
-                    Sign Up
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="logout" className="nav-link">
-                    Log Out
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-        <Outlet />
-      </>
+    var logFlag = (
+      <Link to="logout" className="nav-link">
+        Log Out
+      </Link>
+    );
+  } else {
+    var logFlag = (
+      <Link to="login" className="nav-link">
+        Log In
+      </Link>
     );
   }
-
   return (
     <>
-      <nav
-        className="navbar navbar-expand-lg bg-body-tertiary mt-2 rounded"
-        style={{ backgroundColor: "white" }}
-      >
+      <nav className="navbar navbar-expand-lg bg-body-tertiary rounded mt-2">
         <div className="container-fluid">
-          <Link to="/"></Link>
+          <Link to="/">
+            <p className="logo"></p>
+          </Link>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
@@ -81,11 +46,7 @@ function NavBar() {
                   Sign Up
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link to="login" className="nav-link">
-                  Log In
-                </Link>
-              </li>
+              <li className="nav-item">{logFlag}</li>
             </ul>
           </div>
         </div>
