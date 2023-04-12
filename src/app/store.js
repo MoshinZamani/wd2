e306bc096535b86ca6e62a19f1632e6d77a7efe3
Moshin Zamani <mohsenzamanist@gmail.com>
2023-04-12 14:43:54 +0100

    Clean repo

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import logReducer from "../feature/log/logSlice";
-
-export default configureStore({
-  reducer: {
-    log: logReducer,
-  },
-});
